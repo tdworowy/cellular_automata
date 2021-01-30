dad3e1f915bc72e3a12f04072f38ad536314ed87
Tomasz <dworowytomasz@gmail.com>
2021-01-30 09:33:17 +0100

    refactor

--- web_frontend/twoD.js
+++ web_frontend/twoD.js
@@ -1,17 +1,17 @@
-var formEl = document.getElementById('initGrid');
-var formE2 = document.getElementById('step');
+const formEl = document.getElementById('initGrid');
+const formE2 = document.getElementById('step');
 
 
  function initGrid (event) {
-  var headers = new Headers();
+  const headers = new Headers();
   headers.set('Accept', 'application/json');
 
-  var params = new FormData(document.querySelector('#initGrid'))
+  const params = new FormData(document.querySelector('#initGrid'))
 
   document.getElementById("canvas").width = params.get('width') 
   document.getElementById("canvas").height = params.get('height') 
  
-  var url = new URL('http://localhost:5000/grid/2d/random')
+  const url = new URL('http://localhost:5000/grid/2d/random')
   url.search = new URLSearchParams(params).toString()
  
   var responsePromise = fetch(url,  {method: "GET",
@@ -32,18 +32,18 @@ var formE2 = document.getElementById('step');
 
 
 function step (event) {
-  var headers = new Headers();
+  const headers = new Headers();
   headers.set("Content-Type", "application/json");
 
-  var params = new FormData(document.querySelector('#step'))
-  var grid = JSON.parse(localStorage.getItem("prevGrid"))
+  const params = new FormData(document.querySelector('#step'))
+  const grid = JSON.parse(localStorage.getItem("prevGrid"))
  
-  var body = {
+  let body = {
     'rule':params.get('rule'),
     'grid':grid
   } 
   body = JSON.stringify({...body})
-  var url = new URL('http://localhost:5000/CellularAutomata/2d/step')
+  const url = new URL('http://localhost:5000/CellularAutomata/2d/step')
  
   var responsePromise = fetch(url,  {
     method: "POST",
@@ -68,9 +68,9 @@ const blue = "rgba("+0+","+0+","+255+","+255+")";
 const red = "rgba("+255+","+0+","+0+","+255+")";
 
 function generateGrid(grid){
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext("2d");
-var prevGrid = JSON.parse(localStorage.getItem("prevGrid"));
+ const canvas = document.getElementById("canvas");
+ const context = canvas.getContext("2d");
+ const prevGrid = JSON.parse(localStorage.getItem("prevGrid"));
 if(prevGrid != grid) {
   for(var x=0; x<grid.length;x++) {
     for(var y=0; y<grid[x].length;y++) {
@@ -88,7 +88,7 @@ if(prevGrid != grid) {
 }
 
 async function play(){
-  var count = 1
+  let count = 1
   while(true) {
     await step()
     console.log(`step ${count}`)
