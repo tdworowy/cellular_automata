28c996409da39170a6742f501e885d8adf373c4b
Tomasz <dworowytomasz@gmail.com>
2021-02-13 10:59:46 +0100

    update

--- web_frontend/twoD.js
+++ web_frontend/twoD.js
@@ -1,6 +1,8 @@
 const formEl = document.getElementById('initGrid');
 const formE2 = document.getElementById('step');
 
+const call_width = 10
+const call_hight = 10
 
  function initGrid (event) {
   const headers = new Headers();
@@ -8,8 +10,8 @@ const formE2 = document.getElementById('step');
 
   const params = new FormData(document.querySelector('#initGrid'))
 
-  document.getElementById("canvas").width = params.get('width') 
-  document.getElementById("canvas").height = params.get('height') 
+  document.getElementById("canvas").width = params.get('width') * call_width
+  document.getElementById("canvas").height = params.get('height') * call_hight
  
   const url = new URL('http://localhost:5000/grid/2d/random')
   url.search = new URLSearchParams(params).toString()
@@ -67,24 +69,32 @@ return responsePromise
 const blue = "rgba("+0+","+0+","+255+","+255+")";
 const red = "rgba("+255+","+0+","+0+","+255+")";
 
+
 function generateGrid(grid){
- const canvas = document.getElementById("canvas");
- const context = canvas.getContext("2d");
- const prevGrid = JSON.parse(localStorage.getItem("prevGrid"));
-if(prevGrid != grid) {
-  for(var x=0; x<grid.length;x++) {
-    for(var y=0; y<grid[x].length;y++) {
-      if(grid[x][y] == 1) {
-        var color = red
-      } else {
-        var color = blue
+  const canvas = document.getElementById("canvas");
+  const context = canvas.getContext("2d");
+  const prevGrid = JSON.parse(localStorage.getItem("prevGrid"));
+  let color;
+  let x_cor=0;
+  let y_cor=0;
+  
+  if(prevGrid != grid) {
+    for(var x=0; x < grid.length; x++) {
+      for(var y=0; y < grid[x].length; y++) {
+        if(grid[x][y] == 1) {
+          color = red
+        } else {
+          color = blue
+        }
+        
+        context.fillStyle = color
+        context.fillRect( x_cor, y_cor, call_width, call_hight );
+        x_cor = x * call_width
+        y_cor = y * call_width
       }
-    context.fillStyle = color
-    context.fillRect( x, y, 1, 1 );
-  }
-  }
-  localStorage.setItem("prevGrid", JSON.stringify(grid));
-}    
+    }
+    localStorage.setItem("prevGrid", JSON.stringify(grid));
+  }    
 }
 
 async function play(){
