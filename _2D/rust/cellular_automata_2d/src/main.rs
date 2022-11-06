4f0d3e8502ae94112602cc36c32c14ecc13073d2
Tomasz Dworowy <dworowytomasz@gmail.com>
2022-11-06 11:42:26 +0100

    update

--- _2D/rust/cellular_automata_2d/src/main.rs
+++ _2D/rust/cellular_automata_2d/src/main.rs
@@ -1,4 +1,10 @@
 use rand::Rng;
+use std::collections::HashMap;
+
+fn get_game_of_live_rules() -> HashMap<(u8, u8), u8> {
+    HashMap::from([((0, 3), 1), ((1, 3), 1), ((1, 2), 1)])
+}
+
 fn generate_gird_random(width: usize, height: usize, probability_of_one: f64) -> Vec<Vec<u8>> {
     let mut grid: Vec<Vec<u8>> = Vec::new();
     for i in 0..width {
@@ -30,8 +36,8 @@ fn test_generate_gird_one_cell() {
         [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
     );
 }
-
-fn count_colord_neighbours(x: usize, y: usize, grid: &Vec<Vec<u8>>) -> u8 {
+// TODO need fix 'attempt to subtract with overflow'
+fn count_colored_neighbours(x: usize, y: usize, grid: &Vec<Vec<u8>>) -> u8 {
     let mut count: u8 = 0;
     for i in (x - 1) % grid[0].iter().len()..(x + 2) % grid[0].iter().len() {
         for j in (y - 1) % grid[1].iter().len()..(y + 2) % grid[1].iter().len() {
@@ -44,9 +50,9 @@ fn count_colord_neighbours(x: usize, y: usize, grid: &Vec<Vec<u8>>) -> u8 {
 }
 
 #[test]
-fn test_count_colord_neighbours() {
+fn test_count_colored_neighbours() {
     assert_eq!(
-        count_colord_neighbours(
+        count_colored_neighbours(
             1,
             1,
             &vec![vec![1, 1, 0, 0], vec![0, 0, 1, 0], vec![0, 1, 0, 0],]
@@ -54,7 +60,7 @@ fn test_count_colord_neighbours() {
         4
     );
     assert_eq!(
-        count_colord_neighbours(
+        count_colored_neighbours(
             1,
             1,
             &vec![vec![1, 1, 1, 1], vec![1, 1, 1, 1], vec![1, 1, 1, 1],]
@@ -63,8 +69,30 @@ fn test_count_colord_neighbours() {
     );
 }
 
+fn update_grid(grid: &Vec<Vec<u8>>, rules: HashMap<(u8, u8), u8>) -> Vec<Vec<u8>> {
+    let mut new_grid = grid.clone();
+    for (i, row) in grid.iter().enumerate() {
+        for (j, cell) in row.iter().enumerate() {
+            let live_neighbours = count_colored_neighbours(i, j, &grid);
+            let state = *cell;
+            new_grid[i][j] = *rules.get(&(state, live_neighbours)).clone().unwrap_or(&0);
+        }
+    }
+    new_grid
+}
+#[test]
+fn test_update_grid() {
+    assert_eq!(
+        update_grid(
+            &vec![vec![1, 1, 0, 0], vec![1, 0, 0, 1], vec![0, 0, 1, 0]],
+            get_game_of_live_rules()
+        ),
+        [[1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+    )
+}
+
 fn main() {
     let grid = generate_gird_random(5, 5, 1.0);
     println!("{:?}", &grid);
-    println!("{:?}", count_colord_neighbours(2, 2, &grid));
+    println!("{:?}", count_colored_neighbours(2, 2, &grid));
 }
