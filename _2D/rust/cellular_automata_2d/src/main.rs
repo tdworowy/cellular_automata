00c77d0ae78110f18f31c6a76dc1af7281ddccf4
Tomasz Dworowy <dworowytomasz@gmail.com>
2022-11-06 10:54:26 +0100

    update

--- _2D/rust/cellular_automata_2d/src/main.rs
+++ _2D/rust/cellular_automata_2d/src/main.rs
@@ -11,6 +11,60 @@ fn generate_gird_random(width: usize, height: usize, probability_of_one: f64) ->
     }
     grid
 }
+#[test]
+fn test_generate_gird_random() {
+    assert_eq!(generate_gird_random(2, 2, 0.0), [[0, 0], [0, 0]]);
+    assert_eq!(generate_gird_random(2, 2, 1.0), [[1, 1], [1, 1]]);
+}
+
+fn generate_gird_one_cell(width: usize, height: usize) -> Vec<Vec<u8>> {
+    let mut grid: Vec<Vec<u8>> = vec![vec![0; height]; width];
+    grid[width / 2][height / 2] = 1;
+    grid
+}
+
+#[test]
+fn test_generate_gird_one_cell() {
+    assert_eq!(
+        generate_gird_one_cell(3, 3),
+        [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
+    );
+}
+
+fn count_colord_neighbours(x: usize, y: usize, grid: &Vec<Vec<u8>>) -> u8 {
+    let mut count: u8 = 0;
+    for i in (x - 1) % grid[0].iter().len()..(x + 2) % grid[0].iter().len() {
+        for j in (y - 1) % grid[1].iter().len()..(y + 2) % grid[1].iter().len() {
+            if grid[i][j] == 1 && (i, j) != (x, y) {
+                count += 1;
+            }
+        }
+    }
+    count
+}
+
+#[test]
+fn test_count_colord_neighbours() {
+    assert_eq!(
+        count_colord_neighbours(
+            1,
+            1,
+            &vec![vec![1, 1, 0, 0], vec![0, 0, 1, 0], vec![0, 1, 0, 0],]
+        ),
+        4
+    );
+    assert_eq!(
+        count_colord_neighbours(
+            1,
+            1,
+            &vec![vec![1, 1, 1, 1], vec![1, 1, 1, 1], vec![1, 1, 1, 1],]
+        ),
+        8
+    );
+}
+
 fn main() {
-    println!("{:?}", generate_gird_random(10, 10, 0.3));
+    let grid = generate_gird_random(5, 5, 1.0);
+    println!("{:?}", &grid);
+    println!("{:?}", count_colord_neighbours(2, 2, &grid));
 }
